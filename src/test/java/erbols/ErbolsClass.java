61fe523663ae4ba409291b541e89e63c2eeb1100
bosya03 <erbol.baktiiaruulu@mail.ru>
2025-07-14 19:22:33 -0500

    added second sout

--- src/test/java/erbols/ErbolsClass.java
+++ src/test/java/erbols/ErbolsClass.java
@@ -1,9 +1,11 @@
 package erbols;
 
 public class ErbolsClass {
+
     public static void main(String[] args) {
 
         System.out.println("Erbol's code");
+        System.out.println("Erbol's code NUMBER 2");
 
     }
 }
